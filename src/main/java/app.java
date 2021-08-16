f2399cfa2888059de5f49fe8c64cbec972e43bd8
dfgdfgertt <dfgdfgertt@github.com>
2021-08-16 09:22:34 +0700

    + add count char in string

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -8,9 +8,17 @@ public class app {
         System.out.print("Nhập vào 1 chuỗi ký tự: ");
         String str = input.nextLine();
 
+        //Bai 1
         System.out.println("\n" + str.toUpperCase());
 
+        //Bai 2
         System.out.println("\n" +cutNumeric(str));
+
+        //Bai 3
+        System.out.print("Nhập vào 1 ký tự: ");
+        String c = input.nextLine();
+
+        System.out.println("\n Ky tu " + c + "xuất hiện: " + countLoopString(str,c) + " lần");
     }
 
     public static String cutNumeric(String str){
@@ -25,5 +33,15 @@ public class app {
         str= str.replace("8", "");
         str= str.replace("9", "");
         return str;
+    }
+
+    public static int countLoopString(String a, String b){
+        int i =0;
+        StringBuilder build = new StringBuilder(a);
+        do{
+            i++;
+            build.deleteCharAt(build.indexOf(b));
+        }while (build.indexOf(b) != -1);
+        return i;
     }
 }
